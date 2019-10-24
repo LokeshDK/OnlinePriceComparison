7ddde2352a18429c7a0a1bb5a464adfdde4295bb
LokeshDK <lokesh11292@gmail.com>
2019-10-24 18:29:14 +0100

    Update routes.rb
    changed typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-rRails.application.routes.draw do
+Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
