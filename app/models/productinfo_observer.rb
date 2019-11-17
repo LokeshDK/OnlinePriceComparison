d384f90e0bf837c85e9a33b56d9e45d69a397a50
suryakumargovindan <suryajapan@gmail.com>
2019-11-17 01:52:56 +0000

    gem 3.1.0

--- app/models/productinfo_observer.rb
+++ app/models/productinfo_observer.rb
@@ -5,9 +5,6 @@ class ProductinfoObserver < ActiveRecord::Observer
 # use the MyLogger instance method to retrieve the single instance/object of the class
     @logger = MyLogger.instance
 # use the logger to log/record a message about the updated car
-    @logger.logInformation("###############Observer Demo:#")
-    @logger.logInformation("+++ ProductObserver: The price of #{record.productname}
-has been updated. New price is #{record.productprice}")
-    @logger.logInformation("##############################")
+    @logger.logInformation("+++ ProductObserver: The price of #{record.productname} has been updated. New price is #{record.productprice}")
   end
 end
