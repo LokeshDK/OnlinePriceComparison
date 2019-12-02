26add59feb6d6f16cf546f17a5ad696b18d022d4
suryakumargovindan <suryajapan@gmail.com>
2019-12-02 15:12:20 +0000

    Changes to observers with profile names

--- app/models/productinfo_observer.rb
+++ app/models/productinfo_observer.rb
@@ -7,8 +7,11 @@ class ProductinfoObserver < ActiveRecord::Observer
 # use the logger to log/record a message about the updated car
 #@user = User.find(params[:record.user_id])
     @user = User.find(record.user_id)
-    @logger.logInformation("+++ ProductObserver: The price of #{record.productname} has been updated. New price is #{record.productprice} Updated by #{record.user_id} #{@user.email}")
-    UserMailer.with(user: @user).welcome_email.deliver
+  #@profile = Profile.find(record.user_id)
+    profile_id = Profile.find_by(user_id: record.user_id).id
+    @profile = Profile.find(profile_id)
+    @logger.logInformation("+++ ProductObserver: The price of #{record.productname} has been updated. New price is #{record.productprice} Updated by  #{@profile.firstname} #{@profile.lastname} #{@user.email}")
+      #UserMailer.with(user: @user).welcome_email.deliver
   end
 
   def after_create(record)
