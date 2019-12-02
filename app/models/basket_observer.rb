5e8d89842dc16f8359fa1732b1073356a6bda0dd
suryakumargovindan <suryajapan@gmail.com>
2019-12-02 20:28:18 +0000

    observer for orders

---
+++ app/models/basket_observer.rb
@@ -0,0 +1,27 @@
+require 'my_logger'
+
+class ProductinfoObserver < ActiveRecord::Observer
+
+  def after_update(record)
+# use the MyLogger instance method to retrieve the single instance/object of the class
+    @logger = MyLogger.instance
+
+# use the logger to log/record a message about the updated car
+#@user = User.find(params[:record.user_id])
+    @user = User.find(record.user_id)
+  #@profile = Profile.find(record.user_id)
+    profile_id = Profile.find_by(user_id: record.user_id).id
+    @profile = Profile.find(profile_id)
+    @logger.logInformation("+++ ProductObserver: The price of #{record.productname} has been updated. New price is #{record.productprice} Updated by  #{@profile.firstname} #{@profile.lastname} #{@user.email}")
+    UserMailer.with(user: @user).welcome_email.deliver
+
+  end
+
+  def after_create(record)
+# use the MyLogger instance method to retrieve the single instance/object of the class
+    @logger = MyLogger.instance
+# use the logger to log/record a message about the updated car
+    @logger.logInformation("+++ ProductObserver: A new product #{record.productname} has been created. The price is #{record.productprice}")
+  end
+
+end
