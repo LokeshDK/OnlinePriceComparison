f3d7509740cbfb9a9cfca361580e1d50e9f3b760
suryakumargovindan <suryajapan@gmail.com>
2019-12-05 20:28:39 +0000

    storeinfo test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-<<<<<<< HEAD
-require 'minitest/reporters'
-=======
 require "minitest/reporters"
->>>>>>> b6ee3c66f5110d2c526d8dced9350b18cf00c225
+
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
